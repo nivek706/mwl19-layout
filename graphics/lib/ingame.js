f27a5f80d5a3335a51f961a4e85e23f228c16fcb
Kevin Wells <nivek706@gmail.com>
2017-10-27 23:17:05 -0500

    added sponsor logos and stuff

--- graphics/lib/ingame.js
+++ graphics/lib/ingame.js
@@ -5,3 +5,24 @@ repMatchName.on('change', function(oldValue, newValue){
 	$('#match-name').html(repMatchName.value);
 });
 
+//slideshow section
+var logoImgArray = [
+	"img/logo-rosewill-transparent.png",
+	"img/logo-deck-transparent.png",
+	"img/logo-bequiet.png",
+	"img/logo-visiontek.jpg"
+];
+
+console.log("logoImgArray= " + logoImgArray);
+
+logoIndex = 0;
+setTimeout(runSlideshow, 15000);
+
+function runSlideshow() {
+	logoIndex++;
+	if(logoIndex >= logoImgArray.length){
+		logoIndex = 0;
+	}
+	document.getElementById("sponsor-logo").src = logoImgArray[logoIndex];
+	setTimeout(runSlideshow, 15000);
+}
